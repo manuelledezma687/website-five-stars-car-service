cfd567e7f56d6fd9fb0481ec9ba4f7103c477e72
manuelledezma687 <mledezma@teco.com.ar>
2022-11-21 18:49:02 -0300

    example con_db

---
+++ con_db_example.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost"; 
+$user = ""; 
+$password = ""; 
+$dbname = ""; 
+$id = '';
+$con = mysqli_connect($host, $user, $password,$dbname);
+$method = $_SERVER['REQUEST_METHOD'];
+ 
+ 
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
