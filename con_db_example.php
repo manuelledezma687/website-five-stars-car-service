b0bfac18b0021f419e945edec78a3fa58e270ac7
manuelledezma687 <mledezma@teco.com.ar>
2022-11-26 21:14:48 -0300

    commit from react autocomplete

--- con_db_example.php
+++ con_db_example.php
@@ -6,8 +6,8 @@ $dbname = "";
 $id = '';
 $con = mysqli_connect($host, $user, $password,$dbname);
 $method = $_SERVER['REQUEST_METHOD'];
- 
- 
+
+
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
